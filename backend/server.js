94f200a894a761771403ce2761584de564896e65
rena <renamaryanil@gmail.com>
2026-06-18 17:27:09 +0530

    fixed env backend

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ const telemetryRoutes = require('./routes/telemetryRoutes');
 const dashboardRoutes = require('./routes/dashboardRoutes');
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
 
 // Middleware
 app.use(cors());
